74d266cecf077e9f2533742373354231f52e5da0
ChristopherDelgado <Christopher.delgado.754@my.csun.edu>
2021-11-09 23:03:32 -0800

    finished decryption algorithm, adjusted encryption algorithm to return a key composed of numerical distances used for offsetting each character during decryption

--- decrypt.py
+++ decrypt.py
@@ -1,3 +1,15 @@
-# should decrypt the given message using the given key and return the unencrypted message
-# has a function called a decrypt takes in a encrypted message and a key
-# using the key we decrypt the message and return the unencrypted in a txt file in the same directory as decrypt.py
+def decrypt(cipher_message, key):
+    # unencrypted message
+    result = ''
+    # key split into proper pieces
+    key = key.split(' ')
+    # loop over each character in the cipher and key and decrypt
+    for i in range(len(key)):
+        # cipher_message length = key length so getting both characters at i is safe
+        encrypted_char_ascii = ord(cipher_message[i])
+        key_char_ascii = int(key[i])
+        # decrypting the char by offsetting the encrypted char by the key char
+        unencrypted_char = (encrypted_char_ascii - key_char_ascii) % 256
+        result += chr(unencrypted_char)
+    return result
+
