5842e4989de3ed66cdae8033d504ac1900eff5ef
ChristopherDelgado <Christopher.delgado.754@my.csun.edu>
2021-11-09 21:06:44 -0800

    added abstract of what each file should do

--- decrypt.py
+++ decrypt.py
@@ -1,1 +1,3 @@
 # should decrypt the given message using the given key and return the unencrypted message
+# has a function called a decrypt takes in a encrypted message and a key
+# using the key we decrypt the message and return the unencrypted in a txt file in the same directory as decrypt.py
--- gui.py
+++ gui.py
@@ -1,1 +1,3 @@
 # should build a gui that will allow the user to encrypt or decrypt messages
+# should hold the code to build a simple gui using tkinter and this gui should hold two text fields one for the message
+# and one for the key, and two buttons one for encrypting and one for decrypting
