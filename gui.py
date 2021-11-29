5d23a27a11b3b35ee8c29ba00c70fdc5a40f84ad
ChristopherDelgado <Christopher.delgado.754@my.csun.edu>
2021-11-28 21:55:33 -0800

    created labels for each text field

--- gui.py
+++ gui.py
@@ -16,11 +16,18 @@ window.geometry('600x400')
 # telling all grid columns to center their widgets
 window.grid_columnconfigure(0, weight=1)
 
+# labels
+cipherLabel = tk.Label(window, text='Cipher', height=1, width=6)
+keyLabel = tk.Label(window, text='Key', height=1, width=6)
+decryptLabel = tk.Label(window, text='Decrypted', height=1, width=6)
+
 # encrypted message text field
 cipherField = tk.Text(window, height=1, width=50)
 keyField = tk.Text(window, height=1, width=50, state='disabled')
 decryptField = tk.Text(window, height=1, width=50, state='disabled')
-submitButton = tk.Button(window, text="Decrypt", height=2, width=4)
+
+# submit button
+submitButton = tk.Button(window, text="Decrypt", height=1, width=6)
 
 # tuple of text widgets
 widgets = (cipherField, keyField, decryptField, submitButton)
@@ -28,7 +35,7 @@ widgets = (cipherField, keyField, decryptField, submitButton)
 # adding the list of text widgets to the window
 i = 0
 for widget in widgets:
-    widget.grid(row=i, column=0)
+    widget.grid(row=i, column=1, pady=5)
     # center this widget on the screen
     widget.grid_columnconfigure(1, weight=1)
     i += 1
