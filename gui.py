242b0961422698f7859ce24bf66ecbfcd3111702
ChristopherDelgado <Christopher.delgado.754@my.csun.edu>
2021-11-28 22:19:55 -0800

    gui can now communicate with decryption logic

--- gui.py
+++ gui.py
@@ -3,6 +3,26 @@
 # and one for the key, and two buttons one for encrypting and one for decrypting
 
 import tkinter as tk
+import decrypt
+
+
+# decrypt button callback
+def gui_decrypt():
+    # get our input field
+    cipher = cipherField.get('1.0', 'end-1c')
+    key = keyField.get('1.0', 'end-1c')
+
+    # exit if either of our inputs are empty
+    if not cipher and not key:
+        tk.Label(window, text='ENTER ALL FIELDS', fg="#FF0000").grid(row=i, column=0, pady=5)
+        return
+
+    # decrypt the message
+    result = decrypt.decrypt(cipher, key)
+
+    # show result on screen
+    decryptField.delete('1.0', 'end')
+    decryptField.insert('1.0', 'end')
 
 # creating the window
 window = tk.Tk()
@@ -11,7 +31,7 @@ window = tk.Tk()
 window.title('Extended ASCII Encryption')
 
 # window size
-window.geometry('600x400')
+window.geometry('450x250')
 
 # telling all grid columns to center their widgets
 window.grid_columnconfigure(0, weight=1)
@@ -23,25 +43,21 @@ decryptLabel = tk.Label(window, text='Decrypted', height=1, width=7)
 
 # encrypted message text field
 cipherField = tk.Text(window, height=1, width=50)
-keyField = tk.Text(window, height=1, width=50, state='disabled')
+keyField = tk.Text(window, height=1, width=50, )
 decryptField = tk.Text(window, height=1, width=50, state='disabled')
 
 # submit button
-submitButton = tk.Button(window, text="Decrypt", height=1, width=6)
+submitButton = tk.Button(window, text="Decrypt", height=1, width=6, command=gui_decrypt)
 
 # tuples of label text widgets
-labelWidgets = (cipherLabel, keyLabel, decryptLabel)
-textWidgets = (cipherField, keyField, decryptField, submitButton)
+textWidgets = (cipherLabel, cipherField, keyLabel, keyField, decryptLabel, decryptField, submitButton)
 
 # adding the list of text widgets to the window
 i = 0
 for widget in textWidgets:
-    if i < 3:
-        labelWidgets[i].grid(row=i, column=0)
-
-    widget.grid(row=i, column=1, pady=5)
+    widget.grid(row=i, column=0, pady=5)
     # center this widget on the screen
-    widget.grid_columnconfigure(1, weight=1)
+    widget.grid_columnconfigure(1, weight=3)
     i += 1
 
 # adding the button to the window
