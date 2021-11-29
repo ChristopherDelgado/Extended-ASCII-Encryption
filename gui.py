35400858af14c484b1969561f165ec6292e05028
ChristopherDelgado <Christopher.delgado.754@my.csun.edu>
2021-11-28 21:34:52 -0800

    widgets now show up on screen

--- gui.py
+++ gui.py
@@ -10,5 +10,31 @@ window = tk.Tk()
 # creating the window label
 window.title('Extended ASCII Encryption')
 
+# window size
+window.geometry('600x400')
+
+# telling all grid columns to center their widgets
+window.grid_columnconfigure(0, weight=1)
+
+# encrypted message text field
+cipherField = tk.Text(window, height=1, width=50)
+keyField = tk.Text(window, height=1, width=50, state='disabled')
+decryptField = tk.Text(window, height=1, width=50, state='disabled')
+submitButton = tk.Button(window, text="Decrypt", height=2, width=4)
+
+# tuple of text widgets
+widgets = (cipherField, keyField, decryptField, submitButton)
+
+# adding the list of text widgets to the window
+i = 0
+for widget in widgets:
+    widget.grid(row=i, column=0)
+    # center this widget on the screen
+    widget.grid_columnconfigure(1, weight=1)
+    i += 1
+
+# adding the button to the window
+# decryption handling
+
 # start the window
 window.mainloop()
