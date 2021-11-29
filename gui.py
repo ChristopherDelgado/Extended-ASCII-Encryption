01262184cf0391274726a8eb0e76b12f4b9a516e
ChristopherDelgado <Christopher.delgado.754@my.csun.edu>
2021-11-28 20:45:53 -0800

    window now being created in gui

--- gui.py
+++ gui.py
@@ -1,3 +1,14 @@
 # should build a gui that will allow the user to encrypt or decrypt messages
 # should hold the code to build a simple gui using tkinter and this gui should hold two text fields one for the message
 # and one for the key, and two buttons one for encrypting and one for decrypting
+
+import tkinter as tk
+
+# creating the window
+window = tk.Tk()
+
+# creating the window label
+window.title('Extended ASCII Encryption')
+
+# start the window
+window.mainloop()
