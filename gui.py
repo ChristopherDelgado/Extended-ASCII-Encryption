09a6facde10d4395b38a626fe11febaa4df9fc84
ChristopherDelgado <christopher.delgado.754@my.csun.edu>
2021-12-01 17:06:30 -0800

    decryption box is now completely encapsulated

--- gui.py
+++ gui.py
@@ -6,62 +6,62 @@ import tkinter as tk
 import decrypt
 
 
-# decrypt button callback
-def gui_decrypt():
-    # get our input field
-    cipher = cipherField.get('1.0', 'end-1c')
-    key = keyField.get('1.0', 'end-1c')
+def decryptionBox():
+    # decrypt button callback
+    def gui_decrypt():
+        # get our input field
+        cipher = cipherField.get('1.0', 'end-1c')
+        key = keyField.get('1.0', 'end-1c')
 
-    # exit if either of our inputs are empty
-    if not cipher or not key:
-        tk.Label(window, text='ENTER ALL FIELDS', fg="#FF0000").grid(row=i, column=0, pady=5)
-        return
+        # exit if either of our inputs are empty
+        if not cipher or not key:
+            tk.Label(window, text='ENTER ALL FIELDS', fg="#FF0000").grid(row=i, column=0, pady=5)
+            return
 
-    # decrypt the message
-    result = decrypt.decrypt(cipher, key)
+        # decrypt the message
+        result = decrypt.decrypt(cipher, key)
 
-    # show result on screen
-    decryptField.configure(state='normal')
-    decryptField.delete('1.0', 'end')
-    decryptField.insert('1.0', result)
-    decryptField.configure(state='disabled')
+        # show result on screen
+        decryptField.configure(state='normal')
+        decryptField.delete('1.0', 'end')
+        decryptField.insert('1.0', result)
+        decryptField.configure(state='disabled')
 
+    # creating the window
+    window = tk.Tk()
 
-# creating the window
-window = tk.Tk()
+    # creating the window label
+    window.title('Extended ASCII Encryption')
 
-# creating the window label
-window.title('Extended ASCII Encryption')
+    # window size
+    window.geometry('450x250')
 
-# window size
-window.geometry('450x250')
+    # telling all grid columns to center their widgets
+    window.grid_columnconfigure(0, weight=1)
 
-# telling all grid columns to center their widgets
-window.grid_columnconfigure(0, weight=1)
+    # labels
+    cipherLabel = tk.Label(window, text='Cipher', height=1, width=6)
+    keyLabel = tk.Label(window, text='Key', height=1, width=6)
+    decryptLabel = tk.Label(window, text='Decrypted', height=1, width=7)
 
-# labels
-cipherLabel = tk.Label(window, text='Cipher', height=1, width=6)
-keyLabel = tk.Label(window, text='Key', height=1, width=6)
-decryptLabel = tk.Label(window, text='Decrypted', height=1, width=7)
+    # encrypted message text field
+    cipherField = tk.Text(window, height=1, width=50)
+    keyField = tk.Text(window, height=1, width=50, )
+    decryptField = tk.Text(window, height=1, width=50, state='disabled')  # disabled prevents user from typing here
 
-# encrypted message text field
-cipherField = tk.Text(window, height=1, width=50)
-keyField = tk.Text(window, height=1, width=50, )
-decryptField = tk.Text(window, height=1, width=50, state='disabled')  # disabled prevents user from typing here
+    # submit button
+    submitButton = tk.Button(window, text="Decrypt", height=1, width=6, command=gui_decrypt)
 
-# submit button
-submitButton = tk.Button(window, text="Decrypt", height=1, width=6, command=gui_decrypt)
+    # tuples of label text widgets
+    textWidgets = (cipherLabel, cipherField, keyLabel, keyField, decryptLabel, decryptField, submitButton)
 
-# tuples of label text widgets
-textWidgets = (cipherLabel, cipherField, keyLabel, keyField, decryptLabel, decryptField, submitButton)
+    # adding the list of text widgets to the window
+    i = 0
+    for widget in textWidgets:
+        widget.grid(row=i, column=0, pady=5)
+        # center this widget on the screen
+        widget.grid_columnconfigure(1, weight=3)
+        i += 1
 
-# adding the list of text widgets to the window
-i = 0
-for widget in textWidgets:
-    widget.grid(row=i, column=0, pady=5)
-    # center this widget on the screen
-    widget.grid_columnconfigure(1, weight=3)
-    i += 1
-
-# start the window
-window.mainloop()
+    # start the window
+    window.mainloop()
